24dae1b536687e67e4b545e7bf416c99905013c7
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-10 15:55:08 +0900

    feat: 중복된 메뉴 입력에 대한 검증 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -9,17 +9,35 @@ public class Application {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         int visitDate = requestVisitDate();
 
+        Map<String, Integer> menuNameAndQuantity = getMenuNameAndQuantity();
+    }
+
+    private static Map<String, Integer> getMenuNameAndQuantity() {
+        try {
+            return requestMenuNameAndQuantity();
+        } catch (IllegalArgumentException e) {
+            getMenuNameAndQuantity();
+            return getMenuNameAndQuantity();
+        }
+    }
+
+    private static Map<String, Integer> requestMenuNameAndQuantity() {
+        Map<String, Integer> menuNameAndQuantity = new HashMap<>();
+
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1");
         String stringMenu = Console.readLine();
         String[] splitMenu = stringMenu.split(",");
-        Map<String, Integer> menuNameAndQuantity = new HashMap<>();
         for (String menu : splitMenu) {
             String[] split = menu.split("-");
             String menuName = split[0];
             int menuCount = Integer.parseInt(split[1]);
 
+            if (menuNameAndQuantity.containsKey(menuName)) {
+                throw new IllegalArgumentException("[Error] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
             menuNameAndQuantity.put(menuName, menuCount);
         }
+        return menuNameAndQuantity;
     }
 
     private static int requestVisitDate() {

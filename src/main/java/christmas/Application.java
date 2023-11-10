d40c568b355f9814a46e435aafcb8001a79fdc97
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-10 15:17:29 +0900

    feat: 플래너 시작 문구를 알리는 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,7 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+
     }
 }

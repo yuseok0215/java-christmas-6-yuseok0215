4bddec2eb653dcdf69bd95f1b9b41483036d8cd2
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-10 15:20:48 +0900

    feat: 식당 예상 방문 날짜를 요청하는 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,8 +1,16 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        int visitDate = requestVisitDate();
+    }
 
+    private static int requestVisitDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String stringDate = Console.readLine();
+        return Integer.parseInt(stringDate);
     }
 }

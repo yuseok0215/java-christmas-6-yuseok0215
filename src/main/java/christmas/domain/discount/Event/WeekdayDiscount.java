126633f2b61728dd06b11fb0082a02182c77092d
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-15 17:16:52 +0900

    feat: WeekdayDiscount 테스트 코드 작성

--- src/main/java/christmas/domain/discount/Event/WeekdayDiscount.java
+++ src/main/java/christmas/domain/discount/Event/WeekdayDiscount.java
@@ -8,19 +8,24 @@ public class WeekdayDiscount {
         int discountPrice = 0;
         if (dayOfWeekKorean.equals("일요일") || dayOfWeekKorean.equals("월요일") || dayOfWeekKorean.equals("화요일") || dayOfWeekKorean.equals("수요일")
                 || dayOfWeekKorean.equals("목요일")) {
-            for (Map.Entry<String, Integer> entry : menuNameAndQuantity.entrySet()) {
-                String menuName = entry.getKey();
-                Integer menuQuantity = entry.getValue();
-
-                String menuCategory = Menu.getMenuCategory(menuName);
-
-                if (menuCategory.equals("Dessert")) {
-                    discountPrice += 2023 * menuQuantity;
-                }
-            }
+            discountPrice = checkDessertMenu(menuNameAndQuantity, discountPrice);
         }
         if (discountPrice != 0) {
             discountCategory.put("평일 할인", discountPrice);
+        }
+        return discountPrice;
+    }
+
+    private int checkDessertMenu(Map<String, Integer> menuNameAndQuantity, int discountPrice) {
+        for (Map.Entry<String, Integer> entry : menuNameAndQuantity.entrySet()) {
+            String menuName = entry.getKey();
+            Integer menuQuantity = entry.getValue();
+
+            String menuCategory = Menu.getMenuCategory(menuName);
+
+            if (menuCategory.equals("Dessert")) {
+                discountPrice += 2023 * menuQuantity;
+            }
         }
         return discountPrice;
     }
---
+++ src/test/java/christmas/domain/WeekdayDiscountTest.java
@@ -0,0 +1,65 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import christmas.domain.discount.Event.WeekdayDiscount;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class WeekdayDiscountTest {
+
+    @Test
+    void 평일_할인_이벤트_테스트1() {
+        WeekdayDiscount weekdayDiscount = new WeekdayDiscount();
+
+        Map<String, Integer> menuNameAndQuantity = new HashMap<>();
+        menuNameAndQuantity.put("초코케이크", 2);
+        menuNameAndQuantity.put("아이스크림", 1);
+
+        String dayOfWeekKorean = "월요일";
+        Map<String, Integer> discountCategory = new HashMap<>();
+
+        int discountAmount = weekdayDiscount.weekdayDiscount(menuNameAndQuantity, dayOfWeekKorean, discountCategory);
+
+        assertEquals(2023 * 3, discountAmount);
+        assertTrue(discountCategory.containsKey("평일 할인"));
+        assertEquals(2023 * 3, discountCategory.get("평일 할인"));
+    }
+
+    @Test
+    void 평일_할인_이벤트_테스트2() {
+        WeekdayDiscount weekdayDiscount = new WeekdayDiscount();
+
+        Map<String, Integer> menuNameAndQuantity = new HashMap<>();
+        menuNameAndQuantity.put("양송이수프", 2);
+        menuNameAndQuantity.put("해산물파스타", 1);
+
+        String dayOfWeekKorean = "월요일";
+        Map<String, Integer> discountCategory = new HashMap<>();
+
+        int discountAmount = weekdayDiscount.weekdayDiscount(menuNameAndQuantity, dayOfWeekKorean, discountCategory);
+
+        assertEquals(0, discountAmount);
+        assertFalse(discountCategory.containsKey("평일 할인"));
+    }
+
+    @Test
+    void 평일_할인_이벤트_테스트3() {
+        WeekdayDiscount weekdayDiscount = new WeekdayDiscount();
+
+        Map<String, Integer> menuNameAndQuantity = new HashMap<>();
+        menuNameAndQuantity.put("초코케이크", 2);
+        menuNameAndQuantity.put("아이스크림", 1);
+
+        String dayOfWeekKorean = "토요일";
+        Map<String, Integer> discountCategory = new HashMap<>();
+
+        int discountAmount = weekdayDiscount.weekdayDiscount(menuNameAndQuantity, dayOfWeekKorean, discountCategory);
+
+        assertEquals(0, discountAmount);
+        assertFalse(discountCategory.containsKey("평일 할인"));
+    }
+}
